6f5902ffc62b75decd719495a603849565d1beb7
cirun <cirun@live.com>
2022-09-13 16:27:15 +0200

    add helpers, setting up jquery slick, add slick-theme

--- ckanext/boernetx_theme/plugin.py
+++ ckanext/boernetx_theme/plugin.py
@@ -21,5 +21,9 @@ class BoernetxThemePlugin(plugins.SingletonPlugin):
     def get_helpers(self):
         return {
             "boerne_get_showcases": h.get_showcases,
-            "boerne_recent_datasets": h.recent_dataset
+            "boerne_recent_datasets": h.recent_dataset,
+            "get_value_from_showcase_extras": h.get_value_from_showcase_extras,
+            "get_popular_datasets": h.get_popular_datasets,
+            "get_package_metadata": h.get_package_metadata,
+            "get_thumbnail": h.get_thumbnail
         }
