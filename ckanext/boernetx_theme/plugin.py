618d60407c9dd449aad0333493e87bfa9d96366a
Mohammed Minhajuddin <68331751+minhajuddin2510@users.noreply.github.com>
2025-02-06 07:46:22 +0000

    Fixed images

--- ckanext/boernetx_theme/plugin.py
+++ ckanext/boernetx_theme/plugin.py
@@ -13,8 +13,7 @@ class BoernetxThemePlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('webassets',
-            'boernetx_theme')
+        toolkit.add_resource('webassets','boernetx_theme')
 
     # ITemplateHelpers
 
